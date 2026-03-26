1af7eb5f6b3eb4cab0db0728c9d029d76084c31e
stefanmirandadev <stefanmirandadev@gmail.com>
2026-03-26 15:55:49 +1100

    Update serve-docs.sh to reflect dockerised docs service

--- serve-docs.sh
+++ serve-docs.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Script to serve/re-serve mkdocs documentation
-# Kills any existing mkdocs server and starts a fresh one
+# Documentation is served by the docs container at http://localhost:8080
+# Start it with the rest of the services:
+#
+#   docker-compose up -d
+#
+# To start only the docs container:
+#
+#   docker-compose up -d docs
 
-echo "Stopping any existing mkdocs server on port 8000..."
-lsof -ti:8000 | xargs kill -9 2>/dev/null
-
-echo "Starting mkdocs server..."
-cd documentation && ../.venv/bin/mkdocs serve
-
-# Note: Server will run in foreground. Press Ctrl+C to stop.
-# Documentation available at http://127.0.0.1:8000/
+echo "Documentation is available at http://localhost:8080"
+echo "Make sure the docs container is running: docker-compose up -d docs"
